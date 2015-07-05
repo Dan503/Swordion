7b47741cb12635be3e01a15367105064cf60a215
Daniel Tonon <daniel.tonon.503@gmail.com>
2015-07-05 13:30:27 +1000

    Improvement to PHP src function

--- includes/02-base/functions/01-utilities/src.php
+++ includes/02-base/functions/01-utilities/src.php
@@ -1,10 +1,14 @@
 <?php
+//A function for easily referencing content image src paths
 
-	function src($key, $num) {
-		$srcArray = array(
-			'apcImg' => 'airPowerCourses/image'.$num.'.jpg',
-		);
-		return $srcArray[$key];
+//usage:
+//src('example','girl') >> '/assets/images/content/example/girl.jpg'
+//src('example','boy','png') >> '/assets/images/content/example/boy.png'
+
+	function src($folder, $fileName, $ext) {
+		$basePath = '/assets/images/content/';
+		$ext = isset($ext)? $ext : 'jpg';//default extension is.jpg
+		return $basePath.$folder.'/'.$fileName.'.'.$ext;
 	}
 
 ?>
