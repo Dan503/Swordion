31693cd6350ef6abe5b301453ca1de8568409ab9
Daniel Tonon <daniel.tonon.503@gmail.com>
2015-07-10 14:40:49 +1000

    Improved the "moveTo" function
    - automatically appends to target by default without having to state 'append' as the second argument
    - Can use a string instead of a selector when defining the target (can still use a selector though if that's easier)

--- assets/js/01-global-functions/moveTo.js
+++ assets/js/01-global-functions/moveTo.js
@@ -5,17 +5,24 @@
 	//target is the location it is being moved to
 	//action is the position in relation to the target that the element takes
 
-	//app = append (add it to the end of the target),
-	//prep = prepend (add to the beginning of the target),
-	//before = before (place it just before the target),
-	//after = after (place is straight after the target)
+	//append = add it to the end of the target,
+	//prepend = add to the beginning of the target,
+	//before = place it just before the target,
+	//after = place is straight after the target
 
 	$.fn.moveTo = function(target,action) {
+
+		//if target is a span it will wrap it in the jQuery selector
+		target = typeof target == 'string' ? $(target) : target;
+
+		//The default action is append
+		action = defaultTo(action, 'append');
+
 		switch(action){
-			case "prep": target.prepend(this); break;
-			case "app": target.append(this); break;
+			case "prepend": target.prepend(this); break;
+			case "append": target.append(this); break;
 			case "before": target.before(this); break;
 			case "after": target.after(this); break;
-			case "prep": target.prepend(this); break;
 		}
+		return this;
 	}
