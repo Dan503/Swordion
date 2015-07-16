2f6a33f7de651216d88545fafc8f1eb6f51e2051
Daniel Tonon <daniel.tonon.503@gmail.com>
2015-07-16 18:33:54 +1000

    Improved moveTo function

--- assets/js/01-global-functions/moveTo.js
+++ assets/js/01-global-functions/moveTo.js
@@ -5,17 +5,24 @@
 	//target is the location it is being moved to
 	//action is the position in relation to the target that the element takes
 
-	//app = append (add it to the end of the target),
-	//prep = prepend (add to the beginning of the target),
-	//before = before (place it just before the target),
-	//after = after (place is straight after the target)
+	//append = add it to the end of the target,
+	//prepend = add to the beginning of the target,
+	//before = place it just before the target,
+	//after = place is straight after the target
 
 	$.fn.moveTo = function(target,action) {
-		switch(action){
-			case "prep": target.prepend(this); break;
-			case "app": target.append(this); break;
-			case "before": target.before(this); break;
-			case "after": target.after(this); break;
-			case "prep": target.prepend(this); break;
+
+		//if target is a span it will wrap it in the jQuery selector
+		target = typeof target == 'string' ? $(target) : target;
+
+		if (typeof target != 'undefined') {
+			switch(action){
+				case "prepend": target.prepend(this); break;
+				case "before": target.before(this); break;
+				case "after": target.after(this); break;
+				default : target.append(this); break;//assumes you want to append by default
+			}
 		}
+
+		return this;
 	}
