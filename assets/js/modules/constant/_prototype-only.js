9e7a23814bbc501aee50bd329c883390166d8841
Daniel Tonon <daniel.tonon.503@gmail.com>
2015-07-31 13:39:55 +1000

    added "-JS" to inactive link classes

--- assets/js/modules/constant/_prototype-only.js
+++ assets/js/modules/constant/_prototype-only.js
@@ -6,12 +6,12 @@
 	//gives an inactive class to navigation links that don't go anywhere
 	$('#secondary-nav li a').each(function(){
 		if ($(this).attr('href') == '#') {
-			$(this).addClass('inactive');
+			$(this).addClass('inactive-JS');
 		}
 	});
 	$('#primary-nav li a').each(function(){
 		if ($(this).attr('href') == '#') {
-			$(this).addClass('inactive');
+			$(this).addClass('inactive-JS');
 		}
 	});
 
