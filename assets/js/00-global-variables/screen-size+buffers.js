8b30ba8943d4991a6ee9c0011b60992ace8b3268
Daniel Tonon <daniel.tonon.503@gmail.com>
2015-07-31 13:37:35 +1000

    Updated the syntax of the fullScreen code

--- assets/js/00-global-variables/screen-size+buffers.js
+++ assets/js/00-global-variables/screen-size+buffers.js
@@ -4,21 +4,21 @@
   Always know the current screen width & height
 \************************************************/
 
-var screen_width = $(window).width();
-var screen_height = $(window).height();
+var screen__width = $(window).width();
+var screen__height = $(window).height();
 
 //determines how far down the screen animations start (0.66 = 66% down the screen)
-var x_high_buffer = parseInt(screen_height * 0.25);
-var high_buffer = parseInt(screen_height * 0.33);
-var low_buffer = parseInt(screen_height * 0.66);
-var x_low_buffer = parseInt(screen_height * 0.75);
+var x_high_buffer = parseInt(screen__height * 0.25);
+var high_buffer = parseInt(screen__height * 0.33);
+var low_buffer = parseInt(screen__height * 0.66);
+var x_low_buffer = parseInt(screen__height * 0.75);
 
 $(window).resize(function(){
 	screen_width = $(window).width();
-	screen_height = $(window).height();
+	screen__height = $(window).height();
 
-	x_high_buffer = parseInt(screen_height * 0.25);
-	high_buffer = parseInt(screen_height * 0.33);
-	low_buffer = parseInt(screen_height * 0.66);
-	x_low_buffer = parseInt(screen_height * 0.75);
+	x_high_buffer = parseInt(screen__height * 0.25);
+	high_buffer = parseInt(screen__height * 0.33);
+	low_buffer = parseInt(screen__height * 0.66);
+	x_low_buffer = parseInt(screen__height * 0.75);
 });
--- assets/js/modules/constant/fullScreen.js
+++ assets/js/modules/constant/fullScreen.js
@@ -1,21 +1,31 @@
 ﻿
 //make any element take up the full screen height minus the optional subtracted element
 
+var module_fullScreen = 'fullScreen';
+
+module = module_fullScreen;
+
+moduleTargets[module] = {
+    //js hooks
+    screenFill : module+'__screenFill',
+    subtractor : module+'__subtractor',
+};
+
+
 $.fn.fullScreen = function(subtraction){
 	var _this = this;
 
-	function fillScreen(target){
-		var windowHeight = $(window).height();
-		var subtractionHeight =  typeof subtraction != 'undefined' ? $(subtraction).height() : 0;
 
-		target.height(windowHeight - subtractionHeight);
-	};
-
-	fillScreen(_this);
-
-	$(window).resize(function(){
-		fillScreen(_this)
-	});
 }
+function fillScreen(target){
+	module = module_fullScreen;
+	var subtractionHeight = $(Hook('subtractor')).length ? $(Hook('subtractor')).height() : 0;
 
-$('.js-fullScreen').fullScreen('header');
+	target.height(screen__height - subtractionHeight);
+};
+
+fillScreen(Hook('screenFill'));
+
+$(window).resize(function(){
+	fillScreen(_this)
+});
