80f282cb09f5ec6af2b1a2bad1e32ae1b6ab4414
Daniel Tonon <daniel.tonon.503@gmail.com>
2015-08-11 18:26:49 +1000

    Fix for waitForMe plugin

--- assets/js/plugins/constant/waitForMe.js
+++ assets/js/plugins/constant/waitForMe.js
@@ -1,24 +1,25 @@
 ﻿
 //Wait until an element has loaded before firing off a function.
 //Particularly useful for ajax loaded content ;)
-$.fn.waitForMe = function(callback){
+$.fn.waitForMe = function(callback, loopTime, timeout){
 
-	var element = this;
-	var timeout = 10000;//10 seconds
-	var loopTime = 10;//delay between checks
+	timeout = defaultTo(timeout, 10000);//10 seconds
+	loopTime = defaultTo(loopTime, 10);//delay between checks
+
+	var selection = this.selector;
 	var currentTime = 0;
 
 	var interval = setInterval(function(){
 
 		//element found successfully
-		if (element.length){
+		if ($(selection).length){
 			clearInterval(interval);
-			callback.call(element);
+			callback.call($(selection));
 
 		//element not found within timeout time
 		} else if (currentTime >= timeout){
 			clearInterval(interval);
-			console.log("Error: " + element.selector + " was not found or failed to load.");
+			console.log("Error: " + selection + " was not found or failed to load.");
 		}
 
 		currentTime = currentTime + loopTime;
