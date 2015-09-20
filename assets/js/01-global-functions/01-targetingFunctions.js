c598c8f7a128bd207d2a181f6f18de2c754e0e83
Daniel Tonon <daniel.tonon.503@gmail.com>
2015-09-20 23:24:09 +1000

    Targeting functions upgrade
    They can now accept arrays :)

--- assets/js/01-global-functions/01-targetingFunctions.js
+++ assets/js/01-global-functions/01-targetingFunctions.js
@@ -16,10 +16,23 @@ var moduleTargets = {};
 //returns a CLASS (dot added) eg. ".module-element--modifier-JS"
 var Class = function (key,classSet){
 	classSet = defaultTo(classSet, moduleTargets[module]);
-	return '.'+classSet[key];
+
+	var selectorFull = '';
+
+	if ( key.constructor === Array){
+		$.each(key, function(i, string){
+			var comma = (i == 0) ? '' : ', ';
+			selectorFull = selectorFull + comma + '.'+classSet[string];
+		});
+	} else {
+		selectorFull = '.'+classSet[key];
+	};
+
+	return selectorFull;
 }
 
 //returns a SPAN (nothing added) eg. "module-element--modifier-JS"
+//does not accept arrays
 var Span = function (key,classSet){
 	classSet = defaultTo(classSet, moduleTargets[module]);
 	return classSet[key];
@@ -27,14 +40,42 @@ var Span = function (key,classSet){
 
 //returns a HOOK (an attribute selector)
 var Hook = function(key,classSet){
+
 	classSet = defaultTo(classSet, moduleTargets[module]);
-	return '[data-jshook^="'+classSet[key]+' "], [data-jshook*=" '+classSet[key]+' "], [data-jshook$=" '+classSet[key]+'"], [data-jshook="'+classSet[key]+'"]';
+
+	var selectorFull = '';
+
+//if an array, merge into a single selector
+	if ( key.constructor === Array){
+		$.each(key, function(i, string){
+			var comma = (i == 0) ? '' : ', ';
+			selectorFull = selectorFull + comma + '[data-jshook^="'+classSet[string]+' "], [data-jshook*=" '+classSet[string]+' "], [data-jshook$=" '+classSet[string]+'"], [data-jshook="'+classSet[string]+'"]';
+		});
+//else just output a single copy of the selector
+	} else {
+		selectorFull = '[data-jshook^="'+classSet[key]+' "], [data-jshook*=" '+classSet[key]+' "], [data-jshook$=" '+classSet[key]+'"], [data-jshook="'+classSet[key]+'"]';
+	};
+
+
+	return selectorFull;
 }
 
 //returns an ID (hash added) eg. "#js-module-element"
 var id = function (key,classSet){
 	classSet = defaultTo(classSet, moduleTargets[module]);
-	return '#'+classSet[key];
+
+	var selectorFull = '';
+
+	if ( key.constructor === Array){
+		$.each(key, function(i, string){
+			var comma = (i == 0) ? '' : ', ';
+			selectorFull = selectorFull + comma + '#'+classSet[string];
+		});
+	} else {
+		selectorFull = '#'+classSet[key];
+	};
+
+	return selectorFull;
 };
 
 
