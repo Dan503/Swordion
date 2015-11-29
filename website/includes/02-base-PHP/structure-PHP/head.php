ea1ee1bd7657a949e13bbd73e46c11df3b7dd5af
Daniel Tonon <daniel.tonon.503@gmail.com>
2015-11-29 20:04:45 +1100

    Moved Google analytics tracking to the head of the document
    This is based on Googles recommendation:
    http://stackoverflow.com/questions/10712908/google-analytics-in-header-or-footer

--- website/includes/02-base-PHP/structure-PHP/head.php
+++ website/includes/02-base-PHP/structure-PHP/head.php
@@ -60,10 +60,25 @@
 		}
  	?>
 
+	<!--Google analytics-->
+	<script>
+		/*
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-XXXXXXXX-XX', 'auto');//UA-XXXXXXXX-XX is replaced with Google analitics ID
+		ga('send', 'pageview');
+		*/
+	</script>
+
+	<!--Modernizr-->
 	<script src="<?php echo $rootLocation; ?>/assets/js/vendor-JS/modernizr.2.8.3.min.js"></script>
+
+	<!-- Allows IE8 to style HTML5 elements -->
 	<!--[if lt IE 9]>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
 	<![endif]-->
-	<!-- 1. Allows IE to style HTML5 elements -->
 
 </head>
