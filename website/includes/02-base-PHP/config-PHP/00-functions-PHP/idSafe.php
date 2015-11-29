c9ed6840f2638e47ce2b2fcc94045fc8e5fd26ab
Daniel Tonon <daniel.tonon.503@gmail.com>
2015-11-29 19:21:43 +1100

    idSafe function now generates a guaranteed unique id
    Even if the same string is passed into it multiple times, it will generate a guaranteed unique ID (in relation to the page) every time the function is used

--- website/includes/02-base-PHP/config-PHP/00-functions-PHP/idSafe.php
+++ website/includes/02-base-PHP/config-PHP/00-functions-PHP/idSafe.php
@@ -1,6 +1,22 @@
 <?php
-	function idSafe($string){
+	$GLOBALS['idSafe'] = array();
+
+	//Removes html tags, spaces and special characters. It also adds a random number to the end
+	function generateSafeID($string){
 		$strippedString = strip_tags($string);
-		return preg_replace("/[^A-Za-z0-9]/", "", $strippedString);
+		return preg_replace("/[^A-Za-z0-9]/", "", $strippedString).'-'.rand();
+	}
+
+	function idSafe($string = 'ID'){
+		$id = generateSafeID($string);
+		//If the generated id has already been used, it tries again
+		if (in_array($id, $GLOBALS['idSafe'])){
+			idSafe($string);
+
+		//otherwise it adds it to the list of used id's and returns with the id
+		} else {
+			array_push($GLOBALS['idSafe'], $id);
+			return $id;
+		}
 	}
 ?>
