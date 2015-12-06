66ad597c40ad315df8a37c3a20da4b40abc4cb2c
Daniel Tonon <daniel.tonon@adelphi.digital>
2015-12-06 19:13:09 +1100

    Added clearFix to the site container

--- prototype/includes/01-base-PHP/01-head.php
+++ prototype/includes/01-base-PHP/01-head.php
@@ -112,7 +112,7 @@
 	<?php /* prevents unwanted horizontal scroll bars caused by .grid--gutter-# classes */ ?>
 	<div class="TK-overflowHidden">
 
-		<div class="siteContainer remodal-bg" data-jshook="siteContainer">
+		<div class="siteContainer remodal-bg TK-clearFix" data-jshook="siteContainer">
 			<div class="skipLinks">
 				<a href="#contentStart-sideNav" class="skipLinks__link TK-skipLink" data-jshook="skipLinks__skipToNav skipLinks__link">Skip to side navigation</a>
 				<a href="#contentStart-1" class="skipLinks__link TK-skipLink" data-jshook="skipLinks__link">Skip to content</a>
