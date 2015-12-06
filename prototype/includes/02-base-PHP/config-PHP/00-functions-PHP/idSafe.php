4a6f57789f352567605b14ce0dff46cb020066ce
Daniel Tonon <daniel.tonon@adelphi.digital>
2015-12-06 17:10:26 +1100

    idSafe function now allows number addition to be turned off

--- prototype/includes/02-base-PHP/config-PHP/00-functions-PHP/idSafe.php
+++ prototype/includes/02-base-PHP/config-PHP/00-functions-PHP/idSafe.php
@@ -2,16 +2,24 @@
 	$GLOBALS['idSafe'] = array();
 
 	//Removes html tags, spaces and special characters. It also adds a random number to the end
-	function generateSafeID($string){
+	function generateSafeID($string, $hasNumber){
 		$strippedString = strip_tags($string);
-		return preg_replace("/[^A-Za-z0-9]/", "", $strippedString).'-'.rand();
+		$number = $hasNumber ? '-'.rand() : '';
+
+		return preg_replace("/[^A-Za-z0-9]/", "", $strippedString).$number;
 	}
 
-	function idSafe($string = 'ID'){
-		$id = generateSafeID($string);
-		//If the generated id has already been used, it tries again
+	function idSafe($string = 'ID', $hasNumber = true){
+		$id = generateSafeID($string, $hasNumber);
+
 		if (in_array($id, $GLOBALS['idSafe'])){
-			idSafe($string);
+		//If the generated id has already been used and numbers are enabled, it tries again
+			if ($hasNumber){
+				idSafe($string);
+			} else {
+				//if numbers aren't enabled and it detects a duplicate id, an error will be thrown 
+				throw new Exception('Duplicate ID '.$string.' detected.');
+			}
 
 		//otherwise it adds it to the list of used id's and returns with the id
 		} else {
