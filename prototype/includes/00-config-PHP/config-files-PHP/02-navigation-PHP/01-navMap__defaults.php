179933243bcc2657d92ebfdd4d79aa6eae4c06cd
Daniel Tonon <daniel.tonon@adelphi.digital>
2015-12-07 23:02:19 +1100

    Made a user friendly file for setting default attributes to the navMap

--- prototype/includes/00-config-PHP/config-files-PHP/02-navigation-PHP/01-navMap__defaults.php
+++ prototype/includes/00-config-PHP/config-files-PHP/02-navigation-PHP/01-navMap__defaults.php
@@ -1,68 +1,10 @@
 <?php
-	//set default links for all nav items based on the $navMap variable
 
-function set_link($isActive, $link, $generatedLink){
-	//use the value for 'link' if provided
-	//else use the generated link
-	return defaultTo($link, $generatedLink);
-}
+//Any settings applied here will be applied to every item in the nav map unless overwritten in the navMap itself.
+//Links are generated automatically through the "navMap__applyDefaults" file
 
-function generateLink ($linkOveride, $basePath, $i){
-
-	if (isset($linkOveride)){
-		return $linkOveride;
-
-	} else {
-		$path_exists = file_exists($_SERVER['DOCUMENT_ROOT'].$basePath.$i.'/index.php');
-		$path_levelDown_exists = file_exists($_SERVER['DOCUMENT_ROOT'].$basePath.$i.'/1/index.php');
-		$path_first_exists = file_exists($_SERVER['DOCUMENT_ROOT'].$basePath.'1/index.php');
-
-		echo $basePath.$i.'/
-		';
-
-	   	if ($path_exists) {
-			//Detects if correct file exist and links to it if it does
-			return $basePath.$i.'/';
-
-	   	} else if ($path_levelDown_exists) {
-			//if the correct page doesn't exist, it links to the first page of the next level down
-			return $basePath.$i.'/1/';
-
-		} else if ($path_first_exists) {
-			//if the correct page doesn't exist and the next level down doesn't exist, it links to the first sibling page
-			return $basePath.'1/';
-
-	   	} else {
-			//if none of the above exist, it just leaves the link as a '#'
-			return '#';
-	   	}
-	}
-}
-
-function generateDefaults($basePath, &$map, $index){
-	$recursivePath = $basePath.$index.'/';
-
-    $map['link'] = generateLink($map['link'], $basePath, $index);
-
-	$map = defaultTo($map, array(
-		'isNavigable' => true,//means Item is navigable
-		'subNavigable' => true,//means subnav will appear in the nav
-	));
-
-	//if a subnav exists in item, generate defaults for it
-    if (isset ($map['subnav'])) {
-        foreach ($map['subnav'] as $subIndex => &$subMap) {
-			generateDefaults($recursivePath, $subMap, $subIndex + 1);
-		}
-	}
-}
-
-foreach ($navMap as $i => &$nm) {
-    generateDefaults('/pages/', $nm, $i);
-}
-
-$GLOBALS['navMap'] = $navMap;
-
-var_dump($navMap);
-
+$GLOBALS['navMap__defaults'] = array(
+	'isNavigable' => true,//means Item will appear in the navigation
+	'subNavigable' => true,//means subnav will appear in the navigation
+);
 ?>
