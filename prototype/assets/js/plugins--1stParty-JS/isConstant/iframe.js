c8992b83eefcd3438b5ee10f6750317a4bbab048
Daniel Tonon <daniel.tonon@adelphi.digital>
2015-12-19 23:54:53 +1100

    Removed redundant comment in iframe.js

--- prototype/assets/js/plugins--1stParty-JS/isConstant/iframe.js
+++ prototype/assets/js/plugins--1stParty-JS/isConstant/iframe.js
@@ -2,4 +2,4 @@
 //youtube iframe z-index fix (better to be done in backend code)
 
 //ensures embeded youtube videos obay their z-index
-$("iframe").length&&$("iframe").each(function(){var a=$(this).attr("src");b=-1===a.indexOf("?")?"?":"&";$(this).attr("src",a+b+"wmode=transparent")});//better to do this as back-end code
+$("iframe").length&&$("iframe").each(function(){var a=$(this).attr("src");b=-1===a.indexOf("?")?"?":"&";$(this).attr("src",a+b+"wmode=transparent")});
