b7f19bea1a45f3f9dbdd5e4697de5c482714f038
Daniel Tonon <daniel.tonon@adelphi.digital>
2015-12-19 23:08:51 +1100

    getNavMap function can now search by title string

--- prototype/includes/00-config-PHP/config-files-PHP/02-navigation-PHP/getNavMap.php
+++ prototype/includes/00-config-PHP/config-files-PHP/02-navigation-PHP/getNavMap.php
@@ -1,13 +1,36 @@
 <?php
 
-//function for looking up a specific portion of the navMap using a location array
+//function that retrieves map based on title for getNavMap function
+function getTitleMap($map, $title) {
+	foreach ($map as $i => $item){
+		if (strtolower($item['title']) == strtolower($title)){
+			return $item;
+		} elseif (isset($item['subnav'])){
+			getTitleMap($item['subnav'], $title);
+		}
+	}
+}
 
-function getNavMap($location, $portion = false){
+//function for looking up a specific portion of the navMap using a location array or a title
+
+function getNavMap($locationOrTitle, $portion = false){
 
 	$returnMap = $GLOBALS['navMap'];
 
-	foreach ($location as $i => $index){
-		$returnMap = $i == 0 ? $returnMap[$index] : $returnMap['subnav'][($index - 1)];
+	//code for when a location array is given
+	if (is_array($locationOrTitle) && is_int($locationOrTitle[0])){
+		foreach ($locationOrTitle as $i => $index){
+			$returnMap = $i == 0 ? $returnMap[$index] : $returnMap['subnav'][($index - 1)];
+		}
+
+	//code for when a title is given
+	} elseif (is_string($locationOrTitle)) {
+		$returnMap = getTitleMap($returnMap, $locationOrTitle);
+
+	//throws error if $locationOrTitle variable doesn't make sense
+	} else {
+		$error = $locationOrTitle.' is neither a location array or a title';
+		throw new Exception($error);
 	}
 
 	if ($portion == false) {
