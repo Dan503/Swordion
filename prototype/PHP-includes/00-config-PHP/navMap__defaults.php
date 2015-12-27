e6f5b6fd3d1f0a1aa7fc0f174efd8f5d3a970e9c
Daniel Tonon <daniel.tonon.503@gmail.com>
2015-12-27 20:23:10 +1100

    Added lots of instruction.txt documentation files to folders

---
+++ prototype/PHP-includes/00-config-PHP/navMap__defaults.php
@@ -0,0 +1,9 @@
+<?php
+
+//Any settings applied here will be applied to every item in the nav map unless overwritten in the navMap itself.
+//Links are generated automatically through the "navMap__applyDefaults" file in the do-not-edit part of Swordion
+
+$GLOBALS['navMap__defaults'] = array(
+	'subNavigable' => true,//means that the items subnav will appear in the navigation (this needs to be coded in though, it isn't pre-packaged)
+);
+?>
