a17b603e5fa78b57564daeb00ef4bc29afc87bd8
Daniel Tonon <daniel.tonon@adelphi.digital>
2016-01-22 21:46:55 +1100

    Removing the location reduction
    The site seems to still be working ok. Surely SOMETHING is broken. There has to be. :/

--- prototype/00-navMap.php
+++ prototype/00-navMap.php
@@ -29,7 +29,7 @@
 					'subnav' => array(
 						array(
 							'title' => 'example shortcut',
-							'link' => '?location=1-2-3'
+							'link' => '?location=1-1-2'
 						)
 					)
 				)
--- prototype/ZZ-Swordion-DO-NOT-EDIT/00-config-PHP/config-files-PHP/02-navigation-PHP/03-location-variable.php
+++ prototype/ZZ-Swordion-DO-NOT-EDIT/00-config-PHP/config-files-PHP/02-navigation-PHP/03-location-variable.php
@@ -11,15 +11,6 @@ foreach($location as $i => $integer){
 	$location[$i] = (int)+$integer;
 }
 
-//Reduces all location variable values by 1 except for the first one.
-//This makes it more intuitive when setting the location by letting you start the count from 1 in the folder structure while still making it easy to target the correct item in code.
-//... can sometimes cause some confusion though :/
-	for ($i = 0; $i < count($location); $i++) {
-		if ($i != 0) {//doesn't reduce the first item
-			$location[$i] = $location[$i] - 1;
-		}
-	}
-
 //sets $location as a global variable
 $GLOBALS['location'] = $location;
 $GLOBALS['locationString'] = $locationString;
