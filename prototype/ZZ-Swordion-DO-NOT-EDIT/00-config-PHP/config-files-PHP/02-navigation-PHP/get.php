e164d0391e75b81debf31b20459d06ffd1bf5ad1
Daniel Tonon <daniel.tonon@adelphi.digital>
2016-01-22 23:13:21 +1100

    More work on "get" function

--- prototype/ZZ-Swordion-DO-NOT-EDIT/00-config-PHP/config-files-PHP/02-navigation-PHP/get.php
+++ prototype/ZZ-Swordion-DO-NOT-EDIT/00-config-PHP/config-files-PHP/02-navigation-PHP/get.php
@@ -1,12 +1,48 @@
 <?php
+
+//alters array based on style given
+function gotToParentChild($array, $style){
+	if ($style == 'strict'){
+		return false;
+	} else {
+		array_pop($locationCopy);
+		if ($style == 'lazy'){
+			return $locationCopy;
+		} else {
+			array_push($locationCopy, 0);
+			if(isset(getNavMap($locationCopy))){
+				newLocation($locationCopy);
+			} else {
+				gotToParent($locationCopy);
+			}
+		}
+	}
+}
+
+function newLocation($location, $direction = 'forward', $style = 'deep'){
+	$locationCopy = $location;
+
+	if ($style == 'deep'){//default
+		//will go to every possible page in the order they appear in the navMap
+	} elseif ($style == 'strict'){
+		//will skip over subnav and return false if a nav item doesn't exist
+	} elseif ($style == 'lazy'){
+		//will go up a level when hitting the edges and will skip over sub nav
+	}
+
+	if ($direction = 'forward'){
+		//
+	} elseif ($direction = 'reverse'){
+		return gotToParentChild($location, $style);
+	}
+
+}
+
 //work in progress, this will be an upgrade to the $getCurrent etc variables
 function get($option, $parameter = null){
 
 	$location = $GLOBALS['location'];
-
-	$calcLastIndex = count($location) - 1;
-
-	$lastIndex = $location[$calcLastIndex];
+	$lastIndex = end($location);
 
 	switch($option){
 		case 'depth' :
@@ -14,14 +50,20 @@ function get($option, $parameter = null){
 		break;
 
 		case 'prev' :
-			$location[$calcLastIndex] = $lastIndex - 1;
+			$lastIndex = $lastIndex - 1;
 
+			if ($lastIndex < 0) {
+
+			}
 
 			$returnValue = getNavMap($location);
 		break;
 
 		case 'next' :
-			$location[$calcLastIndex] = $lastIndex + 1;
+			$lastIndex = $lastIndex + 1;
+
+
+
 			$returnValue = getNavMap($location);
 		break;
 
