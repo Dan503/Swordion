eefbf785acaf0bdfe203e992a41980897b473b89
Daniel Tonon <Dan503@users.noreply.github.com>
2016-01-24 16:41:27 +1100

    Finally got getNavMap working again

--- prototype/ZZ-Swordion-DO-NOT-EDIT/00-config-PHP/config-files-PHP/02-navigation-PHP/05-getNavMap.php
+++ prototype/ZZ-Swordion-DO-NOT-EDIT/00-config-PHP/config-files-PHP/02-navigation-PHP/05-getNavMap.php
@@ -3,46 +3,66 @@
 //function that retrieves a map from the navMap variable based on the title attribute
 //function not to be used in regular code
 function getTitleMap($map, $title) {
-	$toReturn = "";
+    $returnValue = [];
+
 	foreach ($map as $item){
 		if (strtolower($item['title']) == strtolower($title)){
-			$toReturn =  $item;
-			return $toReturn;
+            //if provided title matches the current item title,
+			$returnValue =  $item;
 		} elseif (isset($item['subnav'])){
-			$returned = getTitleMap($item['subnav'], $title);
-			if($returned != NULL) return $returned;
+			$newMap = getTitleMap($item['subnav'], $title);
+			if($newMap != NULL) {
+                $returnValue = $newMap;
+			}
 		}
 	}
+
+    if ($returnValue == NULL){
+        //Fires off a notice if title could not be found
+        trigger_error('"'.$title.'" title could not be found in navMap');
+    }
+
+	return $returnValue;
 }
+
+function checkSearchTerm($searchTerm){
+    //Throw an error if neither a string or an interval provided
+    if (!is_int($searchTerm) && !is_string($searchTerm)){
+        trigger_error('"'.$searchTerm.'" is neither a string or an interval. '.$searchTerm.' = <br>');
+        var_dump($searchTerm);
+    }
+};
 
 //function that accepts an array to search for specific sections of the nav map
 //array accepts both numbers and titles
 //function not to be used in regular code
 function getSpecificMap($map, $array){
-    
-    $returnMap = $map;
 
 	foreach ($array as $i => $searchTerm){
 
 		//Throw an error if neither a string or an interval provided
-		if (!is_int($searchTerm) && !is_string($searchTerm)){
-			$error = $searchTerm.' is neither a string or an interval';
-			var_dump($searchTerm);
-			throw new Exception($error);
-		}
+		checkSearchTerm($searchTerm);
 
 		if (is_string($searchTerm)){
-			//if item is a string, do a getTitleMap function on the search term
-			$returnMap = getTitleMap($returnMap, $array[0]);
+			//if item is a string, do a getTitleMap function on the search term using the last map
+            if ($i == 0){
+    			$returnMap = getTitleMap($map, $searchTerm);
+            } else {
+                $returnMap = getTitleMap($returnMap['subnav'], $searchTerm);
+            }
 
 		} elseif(is_int($searchTerm)) {
-			//if first item is a number, return the map at the specified index of the full navMap
-			$returnMap = $returnMap[$searchTerm];
+			//if item is a number, return the map at the specified index of the last map in array
+            if ($i == 0){
+    			$returnMap = $map[$searchTerm];
+            } else {
+    			$returnMap = $returnMap['subnav'][$searchTerm];
+            }
 		}
 
 	}
-    
-    var_dump($returnMap);
+
+    //var_dump($returnMap);
     
 	return $returnMap;
 }
@@ -50,13 +70,13 @@ function getSpecificMap($map, $array){
 //function for looking up a specific portion of the navMap using a location array or a title
 //use this function in your code
 //usage: getNavMap([array of titles/numbers, a single title, or a single number], [*optional* portion to retrieve from return map]);
-function getNavMap($searchTerm, $portion = false){
+function getNavMap($searchTerm, $portion = NULL){
 
 	$returnMap = $GLOBALS['navMap'];
 
 	//code for when an array is given (filters results based on numbers and titles provided in array)
 	 if (is_array($searchTerm)){
-		$returnMap = getSpecificMap($returnMap, $searchTerm);
+    	$returnMap = getSpecificMap($returnMap, $searchTerm);
 
 	//code for when a string is given (searches full nav map for Title that matches)
 	} elseif (is_string($searchTerm)) {
@@ -69,14 +89,14 @@ function getNavMap($searchTerm, $portion = false){
 	//throws error if $searchTerm variable doesn't make sense
 	} else {
 		var_dump($searchTerm);
-		$error = $searchTerm.' could not be resolved';
+		$error = $searchTerm.' is not a string, interval or an array';
 		throw new Exception($error);
 	}
 
-	if ($portion == false) {
-		return $returnMap;
-	} else {
+	if (isset($portion)) {
 		return $returnMap[$portion];
+	} else {
+		return $returnMap;
 	}
 }
 
