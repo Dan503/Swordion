63490e026ee675b784d53553f5032fce9b00cf63
Daniel Tonon <daniel.tonon@adelphi.digital>
2016-02-03 12:30:55 +1100

    Drupal pagination has 5 pages now

--- prototype/PHP-includes/02-modules-PHP/00-function-modules-PHP/Drupal/01-drupalPagination.php
+++ prototype/PHP-includes/02-modules-PHP/00-function-modules-PHP/Drupal/01-drupalPagination.php
@@ -7,12 +7,14 @@ function drupalPagination($settings = array()){
 	<div class="item-list">
 		<ul class="pager">
 			<li class="pager-first first"><a title="Go to first page" href="?location='.$location.'&page=1">first</a></li>
-			<li class="pager-previous"><a title="Go to previous page" href="?location='.$location.'&page=1">previous</a></li>
+			<li class="pager-previous"><a title="Go to previous page" href="?location='.$location.'&page=2">previous</a></li>
 			<li class="pager-item"><a title="Go to page 1" href="?location='.$location.'&page=1">1</a></li>
-			<li class="pager-current">2</li>
-			<li class="pager-item"><a title="Go to page 3" href="?location='.$location.'&page=3">3</a></li>
-			<li class="pager-next"><a title="Go to next page" href="?location='.$location.'&page=3">next</a></li>
-			<li class="pager-last last"><a title="Go to last page" href="?location='.$location.'&page=3">last</a></li>
+			<li class="pager-item"><a title="Go to page 2" href="?location='.$location.'&page=2">2</a></li>
+			<li class="pager-current">3</li>
+			<li class="pager-item"><a title="Go to page 4" href="?location='.$location.'&page=4">4</a></li>
+			<li class="pager-item"><a title="Go to page 5" href="?location='.$location.'&page=5">5</a></li>
+			<li class="pager-next"><a title="Go to next page" href="?location='.$location.'&page=4">next</a></li>
+			<li class="pager-last last"><a title="Go to last page" href="?location='.$location.'&page=5">last</a></li>
 		</ul>
 	</div>';
 }
