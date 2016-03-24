6e1630e2cc967e9d467f9f06783f35a79005eba9
Daniel Tonon <daniel.tonon@adelphi.digital>
2016-03-24 20:49:03 +1100

    Now redirects to the 404 page when location is invalid

--- prototype/404.php
+++ prototype/404.php
@@ -1,5 +1,5 @@
 <?php
 	header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found");
-	$location = array(0, 1, 1);
+	$location = array(0, 0, 0);
 	include $_SERVER['DOCUMENT_ROOT'].'/PHP-includes/ZZ-Swordion-DO-NOT-EDIT/02-base-PHP/template.php';
 ?>
--- prototype/index.php
+++ prototype/index.php
@@ -1,1 +1,13 @@
-<?php include $_SERVER['DOCUMENT_ROOT'].'/PHP-includes/ZZ-Swordion-DO-NOT-EDIT/02-base-PHP/template.php'; ?>
+<?php
+include $_SERVER['DOCUMENT_ROOT'].'/PHP-includes/ZZ-Swordion-DO-NOT-EDIT/02-base-PHP/template.php';
+
+var_dump($get['current']);
+
+//If the page doesn't exist in the nav map, redirect to the 404 page
+if (!isset($get['current'])){ ?>
+
+<script type="text/javascript">
+	window.location="/404.php";
+</script>
+
+<?php } ?>
