81a510f586b41e2626cacc33f671fec77e7d9e1f
Daniel Tonon <daniel.tonon@adelphi.digital>
2016-03-24 21:00:18 +1100

    Added an error message in the 404 url to help with debugging

--- prototype/index.php
+++ prototype/index.php
@@ -1,13 +1,13 @@
 <?php
 include $_SERVER['DOCUMENT_ROOT'].'/PHP-includes/ZZ-Swordion-DO-NOT-EDIT/02-base-PHP/template.php';
 
-var_dump($get['current']);
+var_dump($get['url']);
 
-//If the page doesn't exist in the nav map, redirect to the 404 page
+//If the page doesn't exist in the nav map, redirect to the 404 page with an error message in the URL
 if (!isset($get['current'])){ ?>
-
+<!-- Prototype only -->
 <script type="text/javascript">
-	window.location="/404.php";
+	window.location='/404.php?error='+window.location+'_Does_not_exist_in_the_nav_map';
 </script>
-
+<!-- End prototype only -->
 <?php } ?>
