d70a34b95ad832a730dd48526c199be3f560206e
Daniel Tonon <daniel.tonon@adelphi.digital>
2016-03-24 20:36:14 +1100

    Moved Swordion PHP folder
    I moved it into the php-includes folder to keep the PHP all in one place for the most part

--- prototype/404.php
+++ prototype/404.php
@@ -1,5 +1,5 @@
 <?php
 	header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found");
 	$location = array(0, 1, 1);
-	include $_SERVER['DOCUMENT_ROOT'].'/ZZ-Swordion-DO-NOT-EDIT/02-base-PHP/template.php';
+	include $_SERVER['DOCUMENT_ROOT'].'/PHP-includes/ZZ-Swordion-DO-NOT-EDIT/02-base-PHP/template.php';
 ?>
--- prototype/ZZ-Swordion-DO-NOT-EDIT/02-base-PHP/template.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include $_SERVER['DOCUMENT_ROOT'].'/ZZ-Swordion-DO-NOT-EDIT/00-config-PHP/config-loader.php';
-
-include $template.$get['current']['template'].'.php';
-
-include $base.'foot.php';
-
-?>
