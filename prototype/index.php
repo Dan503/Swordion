c926cfad618aca1325da7a8e9d57f91305f41777
Daniel Tonon <daniel.tonon@adelphi.digital>
2016-03-24 21:20:41 +1100

    removed index var_dump

--- prototype/index.php
+++ prototype/index.php
@@ -1,8 +1,6 @@
 <?php
 include $_SERVER['DOCUMENT_ROOT'].'/PHP-includes/ZZ-Swordion-DO-NOT-EDIT/02-base-PHP/template.php';
 
-var_dump($get['url']);
-
 //If the page doesn't exist in the nav map, redirect to the 404 page with an error message in the URL
 if (!isset($get['current'])){ ?>
 <!-- Prototype only -->
