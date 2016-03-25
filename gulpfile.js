c0f28b3240cac1d17829865a2cfbb19ec2eb532a
Daniel Tonon <Dan503@users.noreply.github.com>
2016-03-25 13:23:18 +1100

    Gulp file sass stuff
    It doesn't compile yet but the file is formatted correctly to compile the sass now

--- gulpfile.js
+++ gulpfile.js
@@ -4,44 +4,50 @@ var gulp = require('gulp');
 // Include Our Plugins
 var jshint = require('gulp-jshint');
 var sass = require('gulp-sass');
+var sassGlob = require('gulp-sass-glob');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var gulpSequence = require('gulp-sequence').use(gulp);
 
 // Lint Task
 gulp.task('lint', function() {
-    return gulp.src('js/*.js')
+    return gulp.src('assets/js/modules/**/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
 
 // Compile Our Sass
-gulp.task('sass', function() {
-    return gulp.src('scss/*.scss')
+var sass_output_files = 'prototype/00-source-files/ZZ-Swordion-DO-NOT-EDIT/sass/output-files/';
+
+function compileSass(src){
+    return gulp.src(sass_output_files+src+'.scss')
+		.pipe(sassGlob())
         .pipe(sass())
-        .pipe(gulp.dest('dist/css'));
+        .pipe(gulp.dest('assets/css'));
+}
+
+gulp.task('sass', function() {
+	compileSass('modern');
+	compileSass('ie9');
+	compileSass('ie8');
 });
 
 // Concatenate & Minify JS
-gulp.task('scripts', function() {
-    return gulp.src('js/*.js')
-        .pipe(concat('all.js'))
-        .pipe(gulp.dest('dist'))
-        .pipe(rename('all.min.js'))
+gulp.task('js', function() {
+    return gulp.src('assets/js/modules/**/*.js')
+        .pipe(concat('merged.js'))
+        .pipe(gulp.dest('assets/js/'))
+        .pipe(rename('merged.min.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('dist/js'));
+        .pipe(gulp.dest('assets/js'));
 });
 
 // Watch Files For Changes
 gulp.task('watch', function() {
-    gulp.watch('js/*.js', ['lint', 'scripts']);
-    gulp.watch('scss/*.scss', ['sass']);
+    gulp.watch('assets/js/modules/**/*.js', ['lint', 'js']);
+    gulp.watch('assets/sass/modules/**/*.scss', ['sass']);
 });
 
-//define the order gulp tasks run in
-gulp.task('task-name', function(callback) {
-  runSequence('task-one', ['tasks','two','run','in','parallel'], 'task-three', callback);
-});
-
-// Default Task
-gulp.task('default', ['lint', 'sass', 'scripts', 'watch']);
+//define the order gulp tasks run in when running default gulp task
+gulp.task('default', gulpSequence(['lint', 'js', 'sass'], 'watch'));
