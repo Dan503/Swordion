7797ccf3b5e633d6d086801b96dc07b2bc1ce3a1
Daniel Tonon <Dan503@users.noreply.github.com>
2016-03-25 15:33:07 +1100

    added a grunt to gulp conversion check list
    Just to help me keep track of what functionality I still need to add

---
+++ grunt-to-gulp-check-list.js
@@ -0,0 +1,19 @@
+/*
+########################################
+# Grunt to Gulp conversion check list: #
+########################################
+
+- done - js-merge
+- x - copy to server
+- done - js-minify
+- x - auto-sprite
+- partial - sass-globbing (need to create the @import paths)
+- done - compile sass
+- x - auto-prefix sass (post-css)
+- x - minify sass (css-nano)
+- x - icomoon unpackager
+- x - copy assets to build folder
+- x - ftp upload
+- partial - watch function
+
+*/
