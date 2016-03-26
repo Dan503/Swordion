2b8d3387c8aaf2bc4d0472c654f21833d3f2852a
Daniel Tonon <Dan503@users.noreply.github.com>
2016-03-26 15:39:00 +1100

    The sass is now compiling

--- gulp/sass.js
+++ gulp/sass.js
@@ -2,17 +2,21 @@
 var gulp = require('gulp');
 
 // load plugins
-var sass = require('gulp-sass');
-var sassGlob = require('gulp-sass-glob');
+var sass = require('gulp-sass');//compiles sass using libSASS
+var sassGlob = require('gulp-sass-glob');//enables the use of sass globbing @import syntax in sass files (only at root level)
+var sourcemaps = require('gulp-sourcemaps');//enables the generation of source maps in the css
 
 // Compile the Sass
 var sass_output_files = 'prototype/00-source-files/ZZ-Swordion-DO-NOT-EDIT/sass/output-files/';
 
 function compileSass(src){
-    return gulp.src(sass_output_files+src+'.scss')
+    return gulp
+		.src(sass_output_files+src+'.scss')
 		.pipe(sassGlob())
-        .pipe(sass())
-        .pipe(gulp.dest('assets/css'));
+		.pipe(sourcemaps.init())
+			.pipe(sass().on('error', sass.logError))
+		.pipe(sourcemaps.write('./source-maps'))
+		.pipe(gulp.dest('prototype/assets/css'));
 }
 
 gulp.task('sass', function() {
