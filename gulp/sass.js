18dc7e6c38e1267de0b6bfb457d1374592457d1b
Daniel Tonon <Dan503@users.noreply.github.com>
2016-03-26 16:47:37 +1100

    Added post-css
    Including:
    - auto-prefixer
    - flexibility

--- gulp/sass.js
+++ gulp/sass.js
@@ -2,19 +2,41 @@
 var gulp = require('gulp');
 
 // load plugins
-var sass = require('gulp-sass');//compiles sass using libSASS
-var sassGlob = require('gulp-sass-glob');//enables the use of sass globbing @import syntax in sass files (only at root level)
-var sourcemaps = require('gulp-sourcemaps');//enables the generation of source maps in the css
+
+//compiles sass using libSASS
+var sass = require('gulp-sass');
+//enables the use of sass globbing @import syntax in sass files (only at root level)
+var sassGlob = require('gulp-sass-glob');
+//enables the generation of source maps in the css
+var sourcemaps = require('gulp-sourcemaps');
+//enables the use of postcss
+var postcss = require('gulp-postcss');
+//automatically adds prefixes
+var autoprefixer = require('autoprefixer');
+//adds the flexibility JS polyfill -js-display: flex; css
+var flexibility = require('postcss-flexibility');
+
+gulp.task('css', function () {
+    return gulp.src('./src/*.css')
+        .pipe(postcss(processors))
+        .pipe(gulp.dest('./dest'));
+});
 
 // Compile the Sass
 var sass_output_files = 'prototype/00-source-files/ZZ-Swordion-DO-NOT-EDIT/sass/output-files/';
 
 function compileSass(src){
+    var processors = [
+        autoprefixer({browsers: ['last 2 versions']}),
+		flexibility()
+    ];
+
     return gulp
 		.src(sass_output_files+src+'.scss')
 		.pipe(sassGlob())
 		.pipe(sourcemaps.init())
 			.pipe(sass().on('error', sass.logError))
+	        .pipe(postcss(processors))
 		.pipe(sourcemaps.write('./source-maps'))
 		.pipe(gulp.dest('prototype/assets/css'));
 }
