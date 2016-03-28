4fce86731bcb83c8b252287a1d8f3c01c8aeb55c
Daniel Tonon <Dan503@users.noreply.github.com>
2016-03-28 19:18:38 +1100

    checklist formatting

--- grunt-to-gulp-check-list.js
+++ grunt-to-gulp-check-list.js
@@ -1,14 +1,14 @@
-/*
-########################################
-# Grunt to Gulp conversion check list: #
-########################################
 
-- done - sass-globbing (need to create the @import paths)
-- done - compile sass
-- done - auto-prefix sass (post-css)
-- done - minify sass (css-nano)
-- done - js-merge
-- done - js-minify
+//########################################\\
+//# Grunt to Gulp conversion check list: #\\
+//########################################\\
+
+//- done - sass-globbing (need to create the @import paths)
+//- done - compile sass
+//- done - auto-prefix sass (post-css)
+//- done - minify sass (css-nano)
+//- done - js-merge
+//- done - js-minify
 - x - add browser-sync with npm php server:
 		https://www.npmjs.com/package/gulp-connect-php
 - x - icomoon unpackager
@@ -16,6 +16,6 @@
 - x - copy to server
 - x - ftp upload
 - x - auto-sprite
-- partial - watch function (won't be done until all is done)
+- partial - watch //(won't be done until all is done)
 
-*/
+
