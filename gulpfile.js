2522ca70fae02231c1e8bfc6705d217836b21275
Daniel Tonon <Dan503@users.noreply.github.com>
2016-03-28 19:19:09 +1100

    Trying to add browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -12,10 +12,11 @@ gulp.task('default', gulpSequence(
 	[//files in array run simultaniously unless they have dependencies
 		'js-hint',
 		'js-compile-minify',
-		'sass-compile-minify'
+		'sass-compile-minify',
+		'start-sync-server'
 	],
 
 //files outside arrays will run one after the other in order
-	'watch'
+	'start-sync-server'
 	//watch takes a while to activate unfortunatly. It won't track changes until the "Finished 'watch'" message appears. Be patient, you only need to run it once per session
 ));
