a36315ec0b97db85c5cc7e929bd18973f888c0bc
Daniel Tonon <daniel.tonon@adelphi.digital>
2016-04-11 00:02:12 +1000

    moved prototype templates list into a php module

--- prototype/PHP-includes/01-base-PHP/head.php
+++ prototype/PHP-includes/01-base-PHP/head.php
@@ -119,27 +119,7 @@
 			<a href="#contentStart-1" class="skipLinks__link TK-skipLink" data-jshook="skipLinks__link">Skip to content</a>
 		</div>
 
-		<!-- prototype only -->
-		<div class="protoTemplates" data-jshook="protoTemplates__target protoTemplates__closer">
-			<div class="protoTemplates__body">
-				<h2 class="protoTemplates__heading">Templates</h2>
-				<span tabindex="0" class="protoTemplates__focus" data-jshook="protoTemplates__focus"></span>
-				<ul class="protoTemplates__list TK-noDots">
-					<?php
-						foreach(get(['templateList'], 'subnav') as $item){
-							$activeClass = strtolower($get['current']['template']) == strtolower($item['title']) ?
-								' protoTemplates__link--isActive' : '';
-							echo '
-							<li class="protoTemplates__item">
-								<a href="'.$item['link'].'" class="btn protoTemplates__link'.$activeClass.'" data-jshook="protoTemplates__link">'.$item['title'].'</a>
-							</li>
-							';
-						}
-					?>
-				</ul>
-			</div>
-		</div>
-		<!-- end prototype only -->
+		<?php include $module.'protoTemplates.php'; ?>
 
 		<div class="siteContainer__inner remodal-bg" data-jshook="siteContainer">
 
