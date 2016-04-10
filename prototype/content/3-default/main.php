d20b00d68f934c5083d15c6cfe17d414b042172f
Daniel Tonon <daniel.tonon@adelphi.digital>
2016-04-10 21:42:29 +1000

    Added the new content folder for the loadContent() function

---
+++ prototype/content/3-default/main.php
@@ -0,0 +1,61 @@
+<h1><?php echo $get['current']['title']; ?></h1>
+
+<h2>Uberiora certe sunt - H2</h2>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Intellegi quidem, ut propter aliam quampiam rem, verbi gratia propter voluptatem, nos amemus; Compensabatur, inquit, cum summis doloribus laetitia. Neminem videbis ita laudatum, ut artifex callidus comparandarum voluptatum diceretur. Duo Reges: constructio interrete. At iam decimum annum in spelunca iacet. Quid igitur dubitamus in tota eius natura quaerere quid sit effectum? </p>
+
+<p>Nam, ut sint illa vendibiliora, haec uberiora certe sunt. Hic ambiguo ludimur. Hic ego: Pomponius quidem, inquam, noster iocari videtur, et fortasse suo iure. Zenonis est, inquam, hoc Stoici. Quae iam oratio non a philosopho aliquo, sed a censore opprimenda est. Idemne potest esse dies saepius, qui semel fuit? Hoc sic expositum dissimile est superiori. Et non ex maxima parte de tota iudicabis? Itaque sensibus rationem adiunxit et ratione effecta sensus non reliquit. </p>
+
+<ul>
+	<li>Est tamen ea secundum naturam multoque nos ad se expetendam magis hortatur quam superiora omnia.</li>
+	<li>Quamquam haec quidem praeposita recte et reiecta dicere licebit.
+		<ul>
+			<li>summum pecudis bonum et hominis</li>
+			<li>dixisset et satis disputatum videretur, in oppidum ad Pomponium perreximus omnes. An haec ab eo non dicuntur
+				<ul>
+					<li>Quod cum ille dixisset et satis disputatum videretur, in oppidum ad Pomponium perreximus omnes. An haec ab eo non dicuntur? Summum ením bonum exposuit</li>
+					<li>Quod cum ille dixisset et satis disputatum videretur, in oppidum ad Pomponium perreximus omnes. An haec ab eo non dicuntur? Summum ením bonum exposuit</li>
+					<li>Quod cum ille dixisset et satis disputatum videretur, in oppidum ad Pomponium perreximus omnes. An haec ab eo non dicuntur? Summum ením bonum exposuit</li>
+				</ul>
+			</li>
+			<li>Nummus in Croesi divitiis obscuratur, pars est tamen divitiarum. Apparet statim, quae sint officia, quae actiones. Suo genere perveniant ad extremum;</li>
+		</ul>
+	</li>
+	<li>Quid ad utilitatem tantae pecuniae?</li>
+	<li>Nec tamen ullo modo summum pecudis bonum et hominis idem mihi videri potest.</li>
+	<li>Scaevola tribunus plebis ferret ad plebem vellentne de ea re quaeri.</li>
+</ul>
+
+<h3>An haec ab eo non dicuntur - H3</h3>
+
+<p>Sed eum qui audiebant, quoad poterant, defendebant sententiam suam. At quanta conantur! Mundum hunc omnem oppidum esse nostrum! Incendi igitur eos, qui audiunt, vides. Ita fit cum gravior, tum etiam splendidior oratio. Nummus in Croesi divitiis obscuratur, pars est tamen divitiarum. Apparet statim, quae sint officia, quae actiones. Suo genere perveniant ad extremum; Quod cum ille dixisset et satis disputatum videretur, in oppidum ad Pomponium perreximus omnes. An haec ab eo non dicuntur? Summum ením bonum exposuit vacuitatem doloris; Quantum Aristoxeni ingenium consumptum videmus in musicis? De ingenio eius in his disputationibus, non de moribus quaeritur. Habent enim et bene longam et satis litigiosam disputationem. </p>
+
+<ol>
+	<li>Est tamen ea secundum naturam multoque nos ad se expetendam magis hortatur quam superiora omnia.</li>
+	<li>Quamquam haec quidem praeposita recte et reiecta dicere licebit.
+		<ol>
+			<li>summum pecudis bonum et hominis</li>
+			<li>dixisset et satis disputatum videretur, in oppidum ad Pomponium perreximus omnes. An haec ab eo non dicuntur
+				<ol>
+					<li>Quod cum ille dixisset et satis disputatum videretur, in oppidum ad Pomponium perreximus omnes. An haec ab eo non dicuntur? Summum ením bonum exposuit</li>
+					<li>Quod cum ille dixisset et satis disputatum videretur, in oppidum ad Pomponium perreximus omnes. An haec ab eo non dicuntur? Summum ením bonum exposuit</li>
+					<li>Quod cum ille dixisset et satis disputatum videretur, in oppidum ad Pomponium perreximus omnes. An haec ab eo non dicuntur? Summum ením bonum exposuit</li>
+				</ol>
+			</li>
+			<li>Nummus in Croesi divitiis obscuratur, pars est tamen divitiarum. Apparet statim, quae sint officia, quae actiones. Suo genere perveniant ad extremum;</li>
+		</ol>
+	</li>
+	<li>Quid ad utilitatem tantae pecuniae?</li>
+	<li>Nec tamen ullo modo summum pecudis bonum et hominis idem mihi videri potest.</li>
+	<li>Scaevola tribunus plebis ferret ad plebem vellentne de ea re quaeri.</li>
+</ol>
+
+<h4>Quoad poterant - H4</h4>
+
+<p>Quae cum magnifice primo dici viderentur, considerata minus probabantur. Respondent extrema primis, media utrisque, omnia omnibus. Praeteritis, inquit, gaudeo. Quae cum praeponunt, ut sit aliqua rerum selectio, naturam videntur sequi; Sin kakan malitiam dixisses, ad aliud nos unum certum vitium consuetudo Latina traduceret. Beatus autem esse in maximarum rerum timore nemo potest. Sed haec nihil sane ad rem; At, si voluptas esset bonum, desideraret. Graece donan, Latine voluptatem vocant. Qua ex cognitione facilior facta est investigatio rerum occultissimarum. </p>
+
+<blockquote>
+	<p>Quantum Aristoxeni ingenium consumptum videmus in musicis? Dolere malum est: in crucem qui agitur, beatus esse non potest. Videamus igitur sententias eorum, tum ad verba redeamus. Piso igitur hoc modo, vir optimus tuique, ut scis, amantissimus. Quid turpius quam sapientis vitam ex insipientium sermone pendere? Tu quidem reddes; Quod cum accidisset ut alter alterum necopinato videremus, surrexit statim. </p>
+</blockquote>
+
+<p>Sic enim censent, oportunitatis esse beate vivere. Haec dicuntur fortasse ieiunius; Nam illud vehementer repugnat, eundem beatum esse et multis malis oppressum. Ita prorsus, inquam; Negabat igitur ullam esse artem, quae ipsa a se proficisceretur; Iubet igitur nos Pythius Apollo noscere nosmet ipsos. Videamus animi partes, quarum est conspectus illustrior; Fatebuntur Stoici haec omnia dicta esse praeclare, neque eam causam Zenoni desciscendi fuisse. Quae in controversiam veniunt, de iis, si placet, disseramus. Quis istud, quaeso, nesciebat? </p>
