01f2e0ca467f75a9ce93cad124b09f36692eeb10
Daniel Tonon <Dan503@users.noreply.github.com>
2016-05-05 15:48:24 +1000

    gulp watch tidy up

--- gulp/watch.js
+++ gulp/watch.js
@@ -5,20 +5,22 @@ var gulp = require('gulp');
 // load plugins
 var browserSync = require('browser-sync');
 
-//Reloads the page when changing PHP files
-gulp.task('php-reload', function(){
-	browserSync.reload();
-});
-
 // create a task that ensures the js tasks are complete before reloading browsers
 gulp.task('js-watch', ['js-hint', 'js-compile-minify'], browserSync.reload());
 
 gulp.task('watch', function(){
-	gulp.watch('prototype/**/*.php', ['php-reload']);
 
+	//Reloads the page when changing PHP files
+	gulp.watch('prototype/**/*.php', function(){
+		browserSync.reload();
+	});
+
+	//compiles JS and reloads page when changing JS files
 	gulp.watch('prototype/00-source-files/02-js/**/*.js', ['js-watch']);
 
+	//compiles css and reloads page when changing scss files
 	gulp.watch(['prototype/00-source-files/**/*.scss','!**/generated-files/**/*.scss'], ['sass-compile-modern']);
 
+	//compiles css, unpackages icons, and reloads page when unpackager contents are changed
 	gulp.watch(['prototype/00-source-files/04-icomoon-unpackager/**/*'], ['icomoon-unpackager']);
 });
