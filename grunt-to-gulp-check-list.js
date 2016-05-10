bde31c44e5fb7c32c7e073f819f7f458def8eab2
Daniel Tonon <Dan503@users.noreply.github.com>
2016-05-10 16:24:17 +1000

    updated check list

--- grunt-to-gulp-check-list.js
+++ grunt-to-gulp-check-list.js
@@ -11,12 +11,13 @@
 //- done - js-minify
 //- done - add browser-sync with npm php server:
 //- done - icomoon unpackager
+//- done - copy assets to build folder
+//- done - auto-sprite (removed from gulp version)
+//- done - watch
+//- done - modernizr
 - x - get browser-sync to fully reload the page on icon unpackaging
 - x - fix icommon unpackage bug that kills browsersync sometimes
-- x - copy assets to build folder
 - x - copy to server
 - x - ftp upload
-- x - auto-sprite
-- partial - watch //(won't be done until all is done)
 
 
