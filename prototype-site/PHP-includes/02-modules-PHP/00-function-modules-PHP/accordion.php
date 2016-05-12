75b3590ec43d0b9caacf36e90e2a2606a1ba7baa
Daniel Tonon <Dan503@users.noreply.github.com>
2016-05-12 20:11:54 +1000

    merged dropMenu JS into accordion js (untested)
    a dropMenu is basically just an accordion that activates on hover. Therefore, the dropMenu code is getting meged into the accordion hover trigger code.

--- prototype-site/PHP-includes/02-modules-PHP/00-function-modules-PHP/accordion.php
+++ prototype-site/PHP-includes/02-modules-PHP/00-function-modules-PHP/accordion.php
@@ -9,21 +9,21 @@ function accordion($settings = []){
 		//if settings are defined in the function, use them, else use ['accordion'] attribute in order of (strongest to weakest): navMap item > template setting > layout setting
 		$settings = defaultTo($settings,
 			templateDefault(['accordion'], [
-				'show' => 'first',
+				'show' => 1,//The 1st item is open on page load, (also accepts "all", "none" or any other number)
 				'standardContent' => true,
-				'autoscroll' => true,
+				'autoscroll' => true,//accepts "true", "false" and "mobile" ("mobile" only scrolls on mobile sized screens)
 				'headings' => ['Heading 1', 'Heading 2', 'Heading 3', 'Heading 4', 'Heading 5'],
+				'style' => 'auto', //also accepts "manual" (auto closes items as you open others, manual must be closed manually)
 			])
 		);
 
-		$autoscroll = $settings['autoscroll'] ? ' data-accordion-autoscroll="true"' : '';
-		$showType = $settings['show'];
 		$standardClass = $settings['standardContent'] ? ' standardContent' : '';
+		$autoScroll = $settings['autoscroll'] ? 'true' : 'false';
 
 //Main accordion HTML starts here
 
 		echo '
-		<ul class="accordion TK-noDots" data-jshook="accordion__reference" data-accordion-show="'.$showType.'"'.$autoscroll.'>
+		<ul class="accordion TK-noDots" data-jshook="accordion__reference" data-accordion-show="'.$settings['show'].'" data-accordion-autoscroll="'.$autoScroll.'">
 		';
 			foreach ($settings['headings'] as $i => $heading) {
 				$id =  idSafe($heading);
@@ -31,12 +31,12 @@ function accordion($settings = []){
 				echo
 				'<li id="accordion__'.$id.'" class="accordion__item block block--noPadding" data-jshook="accordion__item">
 					<h2 class="accordion__heading TK-relative">
-						<a href="#accordion__'.$id.'" class="accordion__headLink" data-jshook="accordion__trigger--auto">
+						<a href="#accordion__'.$id.'" class="accordion__headLink" data-jshook="accordion__trigger--'.$settings['style'].'">
 							<span class="accordion__headText">'.$heading.'</span>
 							<span class="accordion__icon"></span>
 						</a>
 					</h2>
-					<div class="accordion__content TK-jsHide'.$standardClass.'" data-jshook="accordion__content">';
+					<div class="accordion__content TK-jsHide'.$standardClass.'" data-jshook="accordion__revealer">';
 						loadContent('accordion/'.$i.'.php');
 					echo '
 					</div>
